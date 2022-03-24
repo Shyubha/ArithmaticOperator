cfd019d21f31d4670d70863b24163084a56bc7c4
Shyubha <86038837+Shyubha@users.noreply.github.com>
2022-03-24 11:04:59 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystems_ArithmaticOperator_UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        start:
+            Console.WriteLine("Enter First NUMBER");
+            int No1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Second NUMBER");
+            int No2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = No1 + No2;
+            Console.WriteLine("Addition of two No's is" + result);
+
+            int result1 = No1 - No2;
+            Console.WriteLine("Substraction of two No's is" + result1);
+
+            int result2 = No1 * No2;
+            Console.WriteLine("Multiplication of two No's is" + result2);
+
+            int result3 = No1 / No2;
+            Console.WriteLine("Division of two No's is" + result3);
+
+            Console.ReadLine();
+            goto start;
+
+
+
+
+
+
+
+        }
+    }
+}
